a45c4020284713a4511591b13057f8a14761095f
Tzahi <ubuntu@jenkins.payplus.co.il>
2023-02-05 10:59:03 +0200

    a

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const os = require('os');
 
-////#console.log("bogo server starting and listening on 8080...");
+//#console.log("bogo server starting and listening on 8080...");
 
 //var handler = function(request, response) {
 //  console.log("Received request from " + request.connection.remoteAddress);
